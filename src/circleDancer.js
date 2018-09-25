190d1b03f6f15d44247fbee6ae4fe174169d6229
lison12 <wilsonli415@gmail.com>
2018-09-24 21:44:17 -0700

    added two dancers and background image

--- src/circleDancer.js
+++ src/circleDancer.js
@@ -1,18 +1,19 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var makeCircleDancer = function(top, left, timeBetweenSteps) {
   this.oldStep = makeDancer.prototype.step;
+  // console.log(this);
   makeDancer.call(this, top, left, timeBetweenSteps);
-  //this.$node = $('<span class="dancer"></span>');
-  this.$node.append('<img src="boy.png" class="dancer boy"></img>');
+  this.$node.append('<img src="circle.png" class="dancer"></img>');
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   //this.step();
   //return blinkyDancer;
+  this.$node.css({animation: 'rotate-center 5s ease-in-out infinite both', '-webkit-animation': 'rotate-center 5s ease-in-out infinite both'})
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinkyDancer.prototype.step = function() {
+makeCircleDancer.prototype = Object.create(makeDancer.prototype);
+makeCircleDancer.prototype.constructor = makeCircleDancer;
+makeCircleDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
 
   this.oldStep();
@@ -20,5 +21,5 @@ makeBlinkyDancer.prototype.step = function() {
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   // this.$node.css();
-  this.$node.toggle();
+  this.$node.animate();
 };
--- src/dogDancer.js
+++ src/dogDancer.js
@@ -1,18 +1,19 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var makeDogDancer = function(top, left, timeBetweenSteps) {
   this.oldStep = makeDancer.prototype.step;
+  // console.log(this);
   makeDancer.call(this, top, left, timeBetweenSteps);
-  //this.$node = $('<span class="dancer"></span>');
-  this.$node.append('<img src="boy.png" class="dancer boy"></img>');
+  this.$node.append('<img src="dog.png" class="dancer"></img>');
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   //this.step();
   //return blinkyDancer;
+  this.$node.css({animation: 'rotate-90-cw 1.3s cubic-bezier(0.250, 0.460, 0.450, 0.940) infinite both', '-webkit-animation': 'rotate-90-cw 1.3s cubic-bezier(0.250, 0.460, 0.450, 0.940) infinite both'})
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinkyDancer.prototype.step = function() {
+makeDogDancer.prototype = Object.create(makeDancer.prototype);
+makeDogDancer.prototype.constructor = makeDogDancer;
+makeDogDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
 
   this.oldStep();
@@ -20,5 +21,5 @@ makeBlinkyDancer.prototype.step = function() {
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   // this.$node.css();
-  this.$node.toggle();
+  this.$node.animate();
 };
